588797733cd0849e63015d9b63fb3f7df1cf37e0
james young <jmeyoung@gmail.com>
2012-10-27 22:15:54 -0400

    added some test data, implemented redirect

--- scripts/sql/bkp_redirector.sql
+++ scripts/sql/bkp_redirector.sql
@@ -7,14 +7,6 @@ DROP TABLE IF EXISTS bkp_redirector.redirect;
 CREATE TABLE bkp_redirector.redirect (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `uri` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-
-DROP TABLE IF EXISTS bkp_redirector.reporting;
-CREATE TABLE bkp_redirector.reporting  (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `uri_id` int(10) unsigned NOT NULL,
   `count` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
---
+++ scripts/sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO bkp_redirector.redirect SET uri = "http://google.com";
+INSERT INTO bkp_redirector.redirect SET uri = "http://yahoo.com";
+INSERT INTO bkp_redirector.redirect SET uri = "http://cnn.com";
+INSERT INTO bkp_redirector.redirect SET uri = "http://foxnews.com";
+INSERT INTO bkp_redirector.redirect SET uri = "http://msnbc.com";
